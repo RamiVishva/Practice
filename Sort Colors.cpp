d21f78f4a0724cf02eaf35128cbb5a87aad8b885
Vishva Rami <102320115+RamiVishva@users.noreply.github.com>
2022-09-05 17:17:11 +0530

    Add files via upload

---
+++ Sort Colors.cpp
@@ -0,0 +1,12 @@
+// Topic : Array
+// Link :https://leetcode.com/problems/sort-colors/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+    }
+};
