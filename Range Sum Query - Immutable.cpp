a4b944f18b69e5e0bb9e9085969d7652d5f8fc7a
Vishva Rami <102320115+RamiVishva@users.noreply.github.com>
2022-09-20 22:38:08 +0530

    Add files via upload

---
+++ Range Sum Query - Immutable.cpp
@@ -0,0 +1,18 @@
+// Topic : Array
+// Link : https://leetcode.com/problems/range-sum-query-immutable/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class NumArray {
+public:
+    NumArray(vector<int> &nums) : psum(nums.size()+1, 0) {
+        partial_sum( nums.begin(), nums.end(), psum.begin()+1);
+    }
+
+    int sumRange(int i, int j) {
+        return psum[j+1] - psum[i];
+    }
+private:
+    vector<int> psum;    
+};
