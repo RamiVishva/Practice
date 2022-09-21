7ff7468a096f39d8f73383894dbbcdb25fd3b240
Vishva Rami <102320115+RamiVishva@users.noreply.github.com>
2022-09-21 23:48:04 +0530

    Add files via upload

---
+++ Find All Numbers Disappeared in an Array.cpp
@@ -0,0 +1,20 @@
+// Topic : Array
+// Link : https://leetcode.com/submissions/detail/805489157/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int len = nums.size();
+        for(int i=0; i<len; i++) {
+            int m = abs(nums[i])-1; // index start from 0
+            nums[m] = nums[m]>0 ? -nums[m] : nums[m];
+        }
+        vector<int> res;
+        for(int i = 0; i<len; i++) {
+            if(nums[i] > 0) res.push_back(i+1);
+        }
+        return res;
+    }
+};
