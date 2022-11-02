5c6bea125aa0f0d44832b17f531618246a9ef765
Vishva Rami <102320115+RamiVishva@users.noreply.github.com>
2022-11-03 00:13:33 +0530

    Add files via upload

---
+++ Delete Node in a Linked List.cpp
@@ -0,0 +1,21 @@
+//Topic : Linked List
+//Link : https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val=node->next->val;
+        node->next=node->next->next;
+    }
+};
