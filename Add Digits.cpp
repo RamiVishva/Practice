6dd1830a259ea8ff495e8d108351e616feb08a4c
Vishva Rami <102320115+RamiVishva@users.noreply.github.com>
2023-02-28 22:18:04 +0530

    Add files via upload

---
+++ Add Digits.cpp
@@ -0,0 +1,14 @@
+//Topic : Math
+//Link : https://leetcode.com/problems/add-digits/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num == 0) return 0;
+        else if(num % 9 == 0) return 9;
+        else return num % 9;
+    }
+};
