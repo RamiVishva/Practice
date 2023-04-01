02f6095b29e7b11c5d8e0c112ddad8e2d61c3a82
Vishva Rami <102320115+RamiVishva@users.noreply.github.com>
2023-04-01 17:40:30 +0530

    Add files via upload

---
+++ Find the Difference.cpp
@@ -0,0 +1,22 @@
+//Topic : Sorting
+//Link : https://leetcode.com/problems/find-the-difference/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            if(s[i]!=t[i]){
+                return t[i];
+            }
+        }
+        return t[n];
+        
+        
+    }
+};
