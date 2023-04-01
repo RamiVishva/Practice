310a905e845216e4ee3b29148ea17b73a6b1e764
Vishva Rami <102320115+RamiVishva@users.noreply.github.com>
2023-04-01 18:00:01 +0530

    Add files via upload

---
+++ Find Pivot Index.cpp
@@ -0,0 +1,24 @@
+//Topic : Array + Prefix Sum
+//Link : https://leetcode.com/problems/find-pivot-index/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum=0,sum2=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+        }
+        for(int i=0;i<nums.size();i++){
+            if(sum-nums[i]==2*sum2){
+                return i;
+            }
+            sum2+=nums[i];
+        }
+        return-1;
+
+    }
+};
