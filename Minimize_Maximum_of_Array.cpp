ad8ecca2036f5a9ad8720c70a8cade949e270e97
Vishva Rami <102320115+RamiVishva@users.noreply.github.com>
2023-04-05 18:45:47 +0530

    Add files via upload

---
+++ Minimize_Maximum_of_Array.cpp
@@ -0,0 +1,18 @@
+//Topic : Array
+//Link : https://leetcode.com/problems/minimize-maximum-of-array/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimizeArrayValue(vector<int>& nums) {
+        long long sum = 0;
+        long long res = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+            res = max(res,(sum+i)/(i+1));
+        }
+        return (int) res;
+    }
+};
