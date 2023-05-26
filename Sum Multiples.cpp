4c45b0db6a7b94658d32556e4d6aa3e564cb7331
Vishva Rami <102320115+RamiVishva@users.noreply.github.com>
2023-05-26 12:39:19 +0530

    Add files via upload

---
+++ Sum Multiples.cpp
@@ -0,0 +1,18 @@
+//Topic : Math
+//Link : https://leetcode.com/problems/sum-multiples/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int ans=0;
+        for(int i=1; i<=n; i++) {
+            if(i%3 ==0 || i%5 ==0 || i%7 ==0){
+                ans+= i;
+            }
+        }
+        return ans;
+    }
+};
