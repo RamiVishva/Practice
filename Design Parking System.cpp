7bbf7e1a8f45dee00700fa340bb2a3334d065b9d
Vishva Rami <102320115+RamiVishva@users.noreply.github.com>
2023-05-29 14:11:53 +0530

    Add files via upload

---
+++ Design Parking System.cpp
@@ -0,0 +1,42 @@
+//Topic : Design
+//Link : https://leetcode.com/problems/design-parking-system/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ParkingSystem {
+public:
+
+    int b, m, s;
+    ParkingSystem(int big, int medium, int small) {
+        b=big;
+        m=medium;
+        s=small;
+    }
+    
+    bool addCar(int carType) {
+        if(carType==1 && b==0 || carType==2 && m==0 || carType==3 && s==0 ){
+            return false;
+        }
+
+        if(carType==1){
+            b--;
+        }
+
+        if(carType==2){
+            m--;
+        }
+
+        if(carType==3){
+            s--;
+        }
+
+        return true;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
